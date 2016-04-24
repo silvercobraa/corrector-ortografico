4f835a2885e54f658956af9efda5234ee9d2fe03
silvercobraa <silver.cobra1n@gmail.com>
2016-04-24 18:17:16 -0300

    Se delegó el llenado del Trie a la clase SpellChecker.

--- Main.cpp
+++ Main.cpp
@@ -1,30 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <cstdio>
-#include "TrieNode.h"
-#include "Trie.h"
+#include "SpellChecker.h"
+
 
 int main(int argc, char const **argv)
 {
-	std::ifstream archivo_diccionario;
-	archivo_diccionario.open(argv[1]);
-	if (!archivo_diccionario.is_open())
-	{
-		std::cout << "no se pudo abrir el archivo " << argv[1] << std::endl;
-		return -1;
-	}
-	string linea;
-	Trie* t = new Trie();
-	while(!archivo_diccionario.eof())
-	{
-		std::getline(archivo_diccionario, linea);
-		if (linea[0] == '%')
-		{
-			continue;
-		}
-		t->add_word(linea);
-		//std::cout << linea << std::endl;
-	}
+	SpellChecker* sp = new SpellChecker();
+	sp->set_dictionary(argv[1]);
+	Trie* t = sp->get_dicctionary();
 	t->print_trie();
 	std::cout << "Contiene 'comentario': " << t->contains("comentario") << std::endl;
 	while (true)
