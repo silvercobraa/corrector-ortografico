e374b12002aa9d81f2283ee4d9340057522a5c87
silvercobraa <silver.cobra1n@gmail.com>
2016-05-04 14:02:26 -0300

    Se agregaron todas las llamadas escenciales.

--- Main.cpp
+++ Main.cpp
@@ -6,15 +6,21 @@
 
 int main(int argc, char const **argv)
 {
+	std::string NOMBRE_ALUMNOS = "Cesar Bolivar, David Vidal";
+	std::string NOMBRE_ARCHIVO_REVISADO = argv[2];
+	std::string URL_DICCIONARIO = "http://www.winedt.org/dict.html";
+
 	FileHandler* file_handler = new FileHandler(argv[1], argv[2], argv[3]);
-	Trie* t = file_handler->extract_dictionary_file_words();
-	//SpellingChecker* sp = new SpellingChecker();
-	//sp->set_dictionary(argv[1]);
-	//Trie* t = sp->get_dictionary();
+	/*Trie* t = */file_handler->extract_dictionary_file_words();
+
 	//t->print_trie();
-	std::cout << "total words: " << t->get_total_words() << std::endl;
-	SpellingChecker* sc = new SpellingChecker();
-	std::cout << sc->is_suggestion("HOL", "HOLAS") << std::endl;
+	file_handler->write_to_log("Alumnos: " + NOMBRE_ALUMNOS + "\n");
+	file_handler->write_to_log("Nombre archivo revisado: " + NOMBRE_ARCHIVO_REVISADO + "\n");
+	file_handler->write_to_log("URL diccionario usado: " + URL_DICCIONARIO + "\n");
+	file_handler->write_suggestions();
+	file_handler->write_statistics();
+	file_handler->close_all();
+	// loop infinito intencional
 	while (true)
 	{
 
