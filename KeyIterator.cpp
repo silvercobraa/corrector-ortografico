7cd9e39cc321f91328da2575aacef089ccba0160
silvercobraa <silver.cobra1n@gmail.com>
2016-05-04 21:54:43 -0300

    Primer commit.

---
+++ KeyIterator.cpp
@@ -0,0 +1,18 @@
+#include "Trie.h"
+
+Trie::KeyIterator::KeyIterator()
+{
+	// TODO: Implementar constructor.
+}
+
+bool Trie::KeyIterator::has_next()
+{
+	// TODO: Implementar este método.
+	return false;
+}
+
+std::string Trie::KeyIterator::get_next()
+{
+	// TODO: Implementar este método.
+	return NULL;
+}
