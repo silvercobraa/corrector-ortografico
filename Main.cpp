931c6e52979220c9d5dda708fa99774339463127
silvercobraa <silver.cobra1n@gmail.com>
2016-05-12 23:18:59 -0300

    Nada digno de mencionar.

--- Main.cpp
+++ Main.cpp
@@ -6,14 +6,12 @@
 
 int main(int argc, char const **argv)
 {
-	std::string NOMBRE_ALUMNOS = "Cesar Bolivar, David Vidal";
+	std::string NOMBRE_ALUMNOS = "silver cobraa";
 	std::string NOMBRE_ARCHIVO_REVISADO = argv[2];
 	std::string URL_DICCIONARIO = "http://www.winedt.org/dict.html";
 
 	FileHandler* file_handler = new FileHandler(argv[1], argv[2], argv[3]);
-	/*Trie* t = */file_handler->extract_dictionary_file_words();
-
-	//t->print_trie();
+	file_handler->extract_dictionary_file_words();
 	file_handler->write_to_log("Alumnos: " + NOMBRE_ALUMNOS + "\n");
 	file_handler->write_to_log("Nombre archivo revisado: " + NOMBRE_ARCHIVO_REVISADO + "\n");
 	file_handler->write_to_log("URL diccionario usado: " + URL_DICCIONARIO + "\n");
